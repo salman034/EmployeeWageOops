45804d2d1271838c0348c76bdc2467733a460831
unknown <you@example.com>
2022-03-23 11:28:50 +0530

    Added: Checking Employee is Present or Not

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -2,6 +2,11 @@ package com.bl.employeewage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Problems!");
+        int Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == Full_Time)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
