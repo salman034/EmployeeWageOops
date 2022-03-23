73af2c036ea6ba933960b289a2508ba7a9ebc263
unknown <you@example.com>
2022-03-23 11:33:44 +0530

    Added: Checking daily Employee Wage

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -2,11 +2,15 @@ package com.bl.employeewage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int Full_Time = 1;
+        int IS_Full_Time = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage;
+
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == Full_Time)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+        if (empCheck == IS_Full_Time)
+            empHrs = 8;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Daily Employee Wage : " + empWage);
     }
 }
