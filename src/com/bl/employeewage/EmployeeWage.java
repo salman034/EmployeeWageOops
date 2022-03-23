40c63ff76b272be242d03578bf17f553e6a3615a
unknown <you@example.com>
2022-03-23 11:49:18 +0530

    Added: Calculating Wages for a Month

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -2,24 +2,28 @@ package com.bl.employeewage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int IS_Full_Time = 1;
-        int IS_Part_Time = 2;
         int EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0;
-        int empWage = 0;
+        int NUM_OF_WORKING_DAYS = 2;
+        int empHrs, empWage, totalEmpWage = 0;
 
-        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case 1 : IS_Full_Time :
-            empHrs = 8;
-                break;
-            case 2 :IS_Part_Time :
-            empHrs = 4;
-                break;
-            default :
-                empHrs = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case 2:
+                    IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage = empWage + totalEmpWage;
+            System.out.println("Employee Wage : " + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage : " + empWage);
+        System.out.println("Total Emp Wage :" + totalEmpWage);
     }
 }
